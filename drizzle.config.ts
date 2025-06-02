4bd178a82446963cbd9b83ffd2a7c556e2eb0cfa
jmbish04 <jmbish04@gmail.com>
2025-06-02 19:20:45 +0000

    migrations

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -6,7 +6,8 @@ export default defineConfig({
   dialect: "sqlite",
   driver: "d1-http",
   dbCredentials: {
-    wranglerConfigPath: "./wrangler.toml",
-    dbName: "DB",
+    accountId: process.env.CLOUDFLARE_ACCOUNT_ID!,
+    databaseId: "6fd26ae9-6e97-4293-983d-a4a3074f1aad",
+    token: process.env.CLOUDFLARE_API_TOKEN!,
   },
 });
